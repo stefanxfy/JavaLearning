5ddc4052b5624e8ad4868234a27fa57e2ccca186
stefan <502681863@qq.com>
2020-04-06 23:27:42 +0800

    xx

--- src/main/java/com/stefan/proxyLearning/jdkProxy/test.java
+++ src/main/java/com/stefan/proxyLearning/jdkProxy/test.java
@@ -1,17 +1,14 @@
 package com.stefan.proxyLearning.jdkProxy;
 
-import sun.misc.ProxyGenerator;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class test {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 		Star jay = new JayChou();
 		 
         StarProxy proxy = new StarProxy(jay);
@@ -24,10 +21,10 @@ public class test {
 	}
 
 	public static  void test() throws IOException {
-        byte[] fileBytes = ProxyGenerator.generateProxyClass("RealProxy", JayChou.class.getInterfaces());
+/*        byte[] fileBytes = ProxyGenerator.generateProxyClass("RealProxy", JayChou.class.getInterfaces());
         ByteBuffer buffer = ByteBuffer.wrap(fileBytes);
         FileChannel fileChannel = FileChannel.open(Paths.get("com/stefan/proxyLearning/jdkProxy/RealProxy.class"), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-        fileChannel.write(buffer);
+        fileChannel.write(buffer);*/
     }
 
 }
