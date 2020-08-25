42e79d9d37e49a5920beec5a7f1974687f7c3a32
stefan <502681863@qq.com>
2020-08-26 00:08:48 +0800

    ReentrantLock源码结构梳理

--- src/main/java/com/stefan/DailyTest/Test12.java
+++ src/main/java/com/stefan/DailyTest/Test12.java
@@ -4,6 +4,19 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * ReentrantLock实现接口Lock
+ * 有一个内部类Sync，同步器提供所有的实现机制
+ * Sync继承了AbstractQueuedSynchronizer（AQS）
+ * Sync主要函数nonfairTryAcquire()、tryRelease()、newCondition()
+ * 公平锁FairSync、非公平锁NonfairSync，都继承了Sync，并分别实现了lock和tryAcquire
+ *
+ * AbstractQueuedSynchronizer extend  AbstractOwnableSynchronizer
+ * Node 双向队列
+ * ConditionObject 双向队列
+ *
+ * AbstractOwnableSynchronizer   setExclusiveOwnerThread、getExclusiveOwnerThread
+ */
 public class Test12 {
     public static void main(String[] args) throws InterruptedException {
         ReentrantLock lock = new ReentrantLock();
