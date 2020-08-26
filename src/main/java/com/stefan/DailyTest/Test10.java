db454623138e488cc9b288f9831759fd9a02e160
Stefan <502681863@qq.com>
2020-08-26 09:41:13 +0800

    update

--- src/main/java/com/stefan/DailyTest/Test10.java
+++ src/main/java/com/stefan/DailyTest/Test10.java
@@ -1,7 +1,12 @@
 package com.stefan.DailyTest;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 1. FutureTask 实现于Runnable、Future
@@ -10,9 +15,23 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
  */
 public class Test10 {
     public static void main(String[] args) {
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
+//        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
 //        executor.execute();
+//        executor.schedule()
 //        executor.scheduleAtFixedRate();
 //        executor.scheduleWithFixedDelay();
+//        Map<String, Integer> map = new HashMap<>();
+//        int i = map.get("1");
+//        System.out.println(i);
+
+        String s = "sds;dvfvf;sfcx;";
+        System.out.println(s.substring(0,s.length() - 1));
+//        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+//        readWriteLock.readLock().lock();
+//        readWriteLock.writeLock().lock();
+        Boolean b = null;
+        boolean bb = b;
+        System.out.println(bb);
+
     }
 }
